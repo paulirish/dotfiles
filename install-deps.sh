8ca40bee2e579b69e8330de07ef13adcdccb811b
Kareem Saleh <kareem@tribehr.com>
2013-09-06 08:52:40 -0400

    Removed RVM, rnamed code folder to 'Util' and put my username in although I wish my name was paulirish.

--- install-deps.sh
+++ install-deps.sh
@@ -2,10 +2,6 @@
 # the `push` command which copies the github compare URL to my clipboard is heaven
 sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
-# https://rvm.io
-# rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
@@ -13,9 +9,9 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 # https://github.com/rupa/z
 # z, awesome directory auto complete
 # z binary is already referenced from .bash_profile
-cd ~/code
+cd ~/Util
 git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
+chmod +x ~/Util/z/z.sh
 
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
