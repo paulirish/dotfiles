2b7e0ff404e078515bbc9ad7ba22472bd3a287db
Ricardo Rivas G <richistron@gmail.com>
2014-02-22 14:34:01 -0600

    new stuff added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Paul's dotfiles
+# Richistron's dotfiles
 
 [mathias's readme](https://github.com/mathiasbynens/dotfiles/) is awesome. go read it.
 
--- install-deps.sh
+++ install-deps.sh
@@ -7,30 +7,28 @@ bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+curl --progress -L https://get.rvm.io | bash -s stable --ruby
+
+# http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
+# nodejs
+mkdir -p /tmp/nodejs-install && cd /tmp/nodejs-install
+curl --progress http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz | tar zx
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
 npm install -g nave
 
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
-
-
+ruby < (curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # https://github.com/rupa/z
-# z, oh how i love you
+# z binary is already referenced from .bash_profile
 mkdir -p ~/code
 cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-# z binary is already referenced from .bash_profile
-
 
 # https://github.com/dronir/SpotifyControl
 # Spotify Controll Script
@@ -42,20 +40,5 @@ git clone git://github.com/dronir/SpotifyControl.git
 cd ~/code
 git clone git://github.com/jeroenbegyn/VLCControl.git
 
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
-
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
-
