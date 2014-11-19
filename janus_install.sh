fef3410dcd0f52812a5b6f5ffbcca7afe9b76715
Ricardo Rivas G <richistron@gmail.com>
2014-11-19 10:20:53 -0600

    Janus extra plugins added

---
+++ janus_install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# go to https://github.com/carlhuda/janus and install janus
+
+read -p "Did you installed janus already? (Y/N)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo 'creating ~./janus directory'
+  cd ~/
+  mkdir .janus
+  cd .janus
+  git clone https://github.com/terryma/vim-multiple-cursors.git
+  git clone https://github.com/bling/vim-airline
+  git clone https://github.com/mustache/vim-mustache-handlebars.git
+  git clone https://github.com/MattesGroeger/vim-bookmarks
+  git clone https://github.com/Shougo/unite.vim
+else
+  echo 'Please got to https://github.com/carlhuda/janus'
+fi
