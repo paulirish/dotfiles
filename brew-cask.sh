1e32a10486415b8a08aa61e4b0d894b383cbc4ac
Paul Irish <paul.irish@gmail.com>
2015-05-31 14:59:29 -0700

    cask install list update.

--- brew-cask.sh
+++ brew-cask.sh
@@ -11,13 +11,12 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # daily
-brew cask install alfred
-brew cask install divvy
+brew cask install spectacle
 brew cask install dropbox
 brew cask install gyazo
 brew cask install onepassword
 brew cask install rescuetime
-# cask install flux
+brew cask install flux
 
 # dev
 brew cask install iterm2
@@ -32,16 +31,18 @@ brew cask install horndis               # usb tethering
 
 # browsers
 brew cask install google-chrome-canary
-brew cask install firefox-nightly --force
-brew cask install webkit-nightly --force
-brew cask install chromium --force
+brew cask install firefox-nightly
+brew cask install webkit-nightly
+brew cask install chromium
 brew cask install torbrowser
 
 # less often
 brew cask install disk-inventory-x
 brew cask install screenflow4 # 4 specifically not 5.
 brew cask install vlc
-brew cask install keycastr  
+brew cask install gpgtools
+brew cask install licecap
+brew cask install utorrent
 
 
 # Not on cask but I want regardless.
