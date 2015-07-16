f3438e844e3344f41e80ad5138375fd7f893ff7f
Mike Hume <mhumeSF@gmail.com>
2015-07-16 01:27:36 -0700

    spelling

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -92,7 +92,7 @@ fi
 ##############################################################################################################
 ### homebrew!
 
-# (if your maching has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
+# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
 mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
 export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
 
