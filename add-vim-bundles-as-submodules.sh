b7fdeb37c5b9dd0c0b9316b345a0a0e191e52d79
Jan Baer <jan@janbaer.de>
2015-10-15 07:03:59 +0200

    Add vim plugins as submodules

---
+++ add-vim-bundles-as-submodules.sh
@@ -0,0 +1,18 @@
+git submodule add https://github.com/scrooloose/nerdtree ./.vim/bundle/nerdtree
+git submodule add https://github.com/majutsushi/tagbar ./.vim/bundle/tagbar
+git submodule add https://github.com/bling/vim-airline ./.vim/bundle/vim-airline
+git submodule add https://github.com/pangloss/vim-javascript ./.vim/bundle/vim-javascript
+git submodule add https://github.com/mxw/vim-jsx ./.vim/bundle/vim-jsx
+git submodule add https://github.com/klen/python-mode ./.vim/bundle/python-mode
+git submodule add https://github.com/jistr/vim-nerdtree-tabs ./.vim/bundle/vim-nerdtree-tabs
+
+git submodule add https://github.com/plasticboy/vim-markdown ./.vim/bundle/vim-markdown
+git submodule add https://github.com/groenewege/vim-less ./.vim/bundle/vim-less
+git submodule add https://github.com/tpope/vim-fugitive ./.vim/bundle/vim-fugitive
+git submodule add https://github.com/chase/vim-ansible-yaml ./.vim/bundle/vim-ansible-yaml
+git submodule add https://github.com/SirVer/ultisnips ./.vim/bundle/ultisnips
+
+
+git submodule add https://github.com/tomtom/vimtlib ./.vim/bundle/tlib_vim
+git submodule add https://github.com/MarcWeber/vim-addon-mw-utils ./.vim/bundle/vim-addon-mw-utils
+git submodule add https://github.com/garbas/vim-snipmate ./.vim/bundle/vim-snipmate
