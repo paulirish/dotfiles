eede633a0f28bea968695e9eca5908fb6c7c6c2a
Ali Mihandoost <i@ali.md>
2016-03-26 04:00:38 +0430

    Fix bash 4 config permission issue

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -167,7 +167,8 @@ sudo easy_install Pygments
 
 # change to bash 4 (installed by homebrew)
 BASHPATH=$(brew --prefix)/bin/bash
-sudo echo $BASHPATH >> /etc/shells
+#sudo echo $BASHPATH >> /etc/shells
+sudo bash -c 'echo $(brew --prefix)/bin/bash >> /etc/shells'
 chsh -s $BASHPATH # will set for current user only.
 echo $BASH_VERSION # should be 4.x not the old 3.2.X
 # Later, confirm iterm settings aren't conflicting.
