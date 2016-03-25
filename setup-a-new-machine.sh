42bfff3b7fef6b1a690ac5190ea6a94238ad7c34
Ali Mihandoost <i@ali.md>
2016-03-26 00:13:31 +0430

    remove chmod for z.sh

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -143,7 +143,6 @@ npm install --global trash-cli
 
 # github.com/rupa/z   - oh how i love you
 git clone https://github.com/rupa/z.git ~/code/z
-chmod +x ~/code/z/z.sh
 # consider reusing your current .z file if possible. it's painful to rebuild :)
 # z is hooked up in .bash_profile
 
