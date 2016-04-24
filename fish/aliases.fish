52416ebc6f53abe35bbb77656d100c940235a37d
Edditoria <edditoria@gmail.com>
2016-04-24 23:59:25 +0800

    gem update --no-document rather then deprecated switch

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -60,5 +60,4 @@ alias fs="stat -f \"%z bytes\""
 
 # Update installed Ruby gems, Homebrew, npm, and their installed packages
 alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
-alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-rdoc --no-ri'
-
+alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-document'
