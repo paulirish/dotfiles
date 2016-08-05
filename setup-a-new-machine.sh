f8eb1adb61606d6bab8a734a8d401bcfabf3fe8b
Paul Irish <paul.irish@gmail.com>
2016-08-05 14:08:13 -0700

    git-recent moved to solo repo.

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -137,6 +137,13 @@ bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # Type `git open` to open the GitHub page or website for a repository.
 npm install -g git-open
+
+# fancy listing of recent branches
+npm install -g git-recent
+
+# sexy git diffs
+npm install -g diff-so-fancy
+
 # trash as the safe `rm` alternative
 npm install --global trash-cli
 
