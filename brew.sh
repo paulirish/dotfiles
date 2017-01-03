1f01360b55ab4a84c514e26bbc90201167f8d826
Peter Piekarczyk <peterpme@users.noreply.github.com>
2017-01-03 12:41:07 -0600

    Replace `gh` with `hub` since they have been merged together (#86)
    * Replace gh with hub. gh was merged into hub
    * brew.sh comments about `hub`

--- brew.sh
+++ brew.sh
@@ -43,8 +43,8 @@ brew install homebrew/dupes/screen
 # run this script when this file changes guy.
 brew install entr
 
-# github util. imho better than hub
-brew install gh
+# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
+brew install hub
 
 
 # mtr - ping & traceroute. best.
