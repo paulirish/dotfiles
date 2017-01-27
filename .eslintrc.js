edf83bf62a5db1ef1ee1855e21d0174ec49ddcc5
Paul Irish <paul.irish@gmail.com>
2017-01-26 18:02:32 -0800

    eslintrc to eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  "extends": [
+    "plugin:unicorn/recommended",
+    "eslint:recommended"
+  ],
+  
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+
+  "rules": {
+    // 2 == error, 1 == warning, 0 == off
+   
+  }
+};
