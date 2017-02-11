3cb2cf68c14d223a5db02d42155a5761ac352816
Jan Baer <jan@janbaer.de>
2017-02-11 06:34:17 +0100

    linux - shell script to suspend the current session

---
+++ suspend_ubuntu.sh
@@ -0,0 +1,9 @@
+dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
+    "org.freedesktop.login1.Manager.Suspend" boolean:true
+
+
+#dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
+#    "org.freedesktop.login1.Manager.Reboot" boolean:true
+
+#dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
+#    "org.freedesktop.login1.Manager.PowerOff" boolean:true
