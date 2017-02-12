c24ba27c1f6f5d3278a143bef59126d713435ae1
Jan Baer <jan@janbaer.de>
2017-02-12 16:15:31 +0100

    i3 - tweak i3wm

---
+++ .config/i3/lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
