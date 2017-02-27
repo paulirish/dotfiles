9a640dd3159c3926d8abd32cdd87d221cccc8266
Paul Irish <paul.irish@gmail.com>
2017-02-27 12:07:07 -0800

    migration folders.

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -7,18 +7,19 @@
 ##############################################################################################################
 ###  backup old machine's key items
 
-mkdir -p ~/migration/home 
-mkdir -p ~/migration/Application\ Support
+mkdir -p ~/migration/home/
+mkdir -p ~/migration/Library/"Application Support"/
 mkdir -p ~/migration/Library/Preferences/
 mkdir -p ~/migration/Library/Application Support/
-mkdir -p ~/migration/rootLibrary/Preferences
+mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
 
 cd ~/migration
 
 # what is worth reinstalling?
-brew leaves      		> brew-list.txt    # all top-level brew installs
-brew cask list 			> cask-list.txt
-npm list -g --depth=0 	> npm-g-list.txt
+brew leaves              > brew-list.txt    # all top-level brew installs
+brew cask list           > cask-list.txt
+npm list -g --depth=0    > npm-g-list.txt
+yarn global ls --depth=0 > yarn-g-list.txt
 
 # then compare brew-list to what's in `brew.sh`
 #   comm <(sort brew-list.txt) <(sort brew.sh-cleaned-up)
@@ -38,7 +39,7 @@ cp -Rp \
 
 cp -Rp ~/Documents ~/migration
 
-cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration  # wifi
+cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
 
 cp -Rp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration/Library/Preferences/
 cp -Rp ~/Library/Preferences/com.tinyspeck.slackmacgap.plist ~/migration/Library/Preferences/
@@ -47,8 +48,8 @@ cp -Rp ~/Library/Services ~/migration/Library/ # automator stuff
 cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
 
 # editor settings & plugins
-cp -Rp "~/Library/Application Support/Sublime Text 3" ~/migration/Library/Application Support/
-cp -Rp ~/Library/Application\ Support/Code\ -\ Insiders/ ~/migration/Library/Application Support/
+cp -Rp ~/Library/Application\ Support/Sublime\ Text\ 3 ~/migration/Library/"Application Support"/
+cp -Rp ~/Library/Application\ Support/Code\ -\ Insiders/ ~/migration/Library/"Application Support"/
 
 # also consider...
 # random git branches you never pushed anywhere?
