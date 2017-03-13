423d12de3a443b8ff60b4f4ba7fc909792392494
Troy Harvey <troy.harvey@generationtux.com>
2017-03-12 23:38:46 -0400

    Update pip packages list

--- pip.sh
+++ pip.sh
@@ -8,5 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+pip install -U pip
+pip install pipenv
+pip install setuptools
 pip install virtualenv
+pip install virtualenv-clone
 pip install virtualenvwrapper
