9773f58d732036958fa903c885ab58249ab2ff8d
Jan Baer <jan@janbaer.de>
2017-03-13 05:46:07 +0100

    Veracrypt - When unmounting, we ask if any data has changes and change date of the container in case of

--- bin/unmount-mydocuments.sh
+++ bin/unmount-mydocuments.sh
@@ -1,1 +1,8 @@
 veracrypt -d /media/MyDocuments
+
+echo -n "Have you change any data in the container (y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+   touch ~/Secure/MyDocuments.tc
+fi
+
--- bin/unmount-xxx-kg.sh
+++ bin/unmount-xxx-kg.sh
@@ -1,1 +1,8 @@
 veracrypt -d /media/XXX-KG
+
+echo -n "Have you change any data in the container (y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+   touch ~/Secure/XXX-KG.tc
+fi
+
