a5b2b653060193db65097de70fee7fb73271c44d
Paul Irish <paul.irish@gmail.com>
2017-04-26 17:27:10 -0700

    chromium.fish edits.

--- fish/chromium.fish
+++ fish/chromium.fish
@@ -2,10 +2,12 @@ function deps --description "run gclient sync"
     env GYP_DEFINES=disable_nacl=1 gclient sync --jobs=70
 end
 
+function hooks --description "run gclient runhooks"
+    env GYP_DEFINES=disable_nacl=1 gclient runhooks
+end
+
 function b --description "build chromium"
 	set -l dir $HOME/chromium/src/out/Default
-	# because crbug.com/695864
-	ninja -C $dir -j1 generate_bindings_modules_v8_interfaces generate_bindings_core_v8_interfaces
 	# 1000 seems fairly stable, but i dont want accidental failures
     ninja -C $dir -j900 chrome blink_tests
 end
@@ -24,10 +26,22 @@ end
 
 function depsbcr --description "deps, then build chromium, then open it"
     if deps
+        # #     if [ "$argv[1]" = "--skipgoma" ] ...
+        gom
+        bcr
+    end
+end
+
+function hooksbcr --description "run hooks, then build chromium, then open it"
+    if hooks
+        gom
         bcr
     end
 end
 
 function gom --description "run goma setup"
-	env GOMAMAILTO=/dev/null env GOMA_OAUTH2_CONFIG_FILE=/Users/paulirish/.goma_oauth2_config env GOMA_ENABLE_REMOTE_LINK=yes ~/goma/goma_ctl.py ensure_start
+    set -x GOMAMAILTO /dev/null 
+    set -x GOMA_OAUTH2_CONFIG_FILE /Users/paulirish/.goma_oauth2_config 
+    set -x GOMA_ENABLE_REMOTE_LINK yes
+    ~/goma/goma_ctl.py ensure_start
 end	
