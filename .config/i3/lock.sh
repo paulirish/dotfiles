7cd175cf63dec5d4d659adfcda3122cdfe027d00
Jan Baer <jan@janbaer.de>
2017-08-02 18:22:45 +0200

    i3wm - Configure lock screen and lock before suspend

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -3,5 +3,5 @@ scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i /tmp/screen.png
+i3lock -i /tmp/screen.png -d -f
 rm /tmp/screen.png
--- .config/i3/suspend_ubuntu.sh
+++ .config/i3/suspend_ubuntu.sh
@@ -1,6 +1,5 @@
-dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
+i3lock -f && dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
     "org.freedesktop.login1.Manager.Suspend" boolean:true
-
 
 #dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
 #    "org.freedesktop.login1.Manager.Reboot" boolean:true
