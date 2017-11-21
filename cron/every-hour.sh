6476e86980984dab3612ac406c5cd15ab4c7a465
Paul Irish <paul.irish@gmail.com>
2017-11-21 11:38:42 -0800

    minor: cron maintenance

--- cron/every-hour.sh
+++ cron/every-hour.sh
@@ -8,9 +8,14 @@ PATH=/Users/paulirish/.homebrew/bin:/Users/paulirish/.homebrew/sbin:/Users/pauli
 
 local_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+which node
+node --version
+
 # https://github.com/ChromeDevTools/devtools-protocol
 cd "$HOME/code/pristine/devtools-protocol/scripts" && ./update-to-latest.sh
 cd "$HOME/code/pristine/devtools-protocol/scripts" && ./update-n-publish-docs.sh
 
 # https://github.com/ChromeDevTools/devtools-frontend
 cd "$HOME/code/npm-publish-devtools-frontend" && ./update-github-mirror.sh
+
