507af4ccf63ce51d3654764d626534faf11b43dc
Jan Baer <jan@janbaer.de>
2017-12-17 17:56:32 +0100

    system - Remove share from mounts

--- bin/mount-nas.sh
+++ bin/mount-nas.sh
@@ -4,6 +4,6 @@ SERVER_ADDRESS=JABASOFT-DS
 USER=jan
 PASSWORD=$(pass /home/JABASOFT-DS/jan)
 
-for SHARE in video music daten photo XXX; do
+for SHARE in video music daten photo; do
   sudo mount -t cifs //$SERVER_ADDRESS/$SHARE /media/$SHARE -o username=$USER,password=$PASSWORD
 done
--- bin/unmount-nas.sh
+++ bin/unmount-nas.sh
@@ -2,6 +2,6 @@
 
 SERVER_ADDRESS=JABASOFT-DS
 
-for SHARE in video music daten photo XXX; do
+for SHARE in video music daten photo; do
   sudo umount /media/$SHARE
 done
