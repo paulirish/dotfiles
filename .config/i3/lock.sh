664665426bbf880acb4effcd5eb4305e4b1a1a5f
Jan Baer <jan@janbaer.de>
2017-12-22 19:34:46 +0100

    i3 - improve lock.sh with a better screenshot bluring

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png -d -f
-rm /tmp/screenshot.png
+SCREENSHOT=/tmp/screenshot.png
+LOCK_IMAGE=~/.config/i3/lock-screen.png
+
+scrot $SCREENSHOT
+convert $SCREENSHOT -blur 0x5 -scale 10% -scale 1000% $SCREENSHOT
+convert $SCREENSHOT $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
+
+i3lock -i $SCREENSHOT -d -f
+rm $SCREENSHOT
+
