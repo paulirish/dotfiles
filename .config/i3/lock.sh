16d694eb153e5be35fa7ad1be5961ce00bea2bc5
Jan Baer <jan@janbaer.de>
2018-01-05 14:30:36 +0100

    i3 - Implement manual monitor turn off after the feature was removed from i3lock

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+revert() {
+  xset +dpms dpms 0 0 0
+}
+
 SCREENSHOT=/tmp/screenshot.png
 LOCK_IMAGE=~/.config/i3/lock-screen.png
 
@@ -6,6 +11,11 @@ scrot $SCREENSHOT
 convert $SCREENSHOT -blur 0x5 -scale 10% -scale 1000% $SCREENSHOT
 convert $SCREENSHOT $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
 
-i3lock -i $SCREENSHOT -d -f
+trap revert HUP INT TERM
+xset +dpms dpms 5 5 5
+
+i3lock -i $SCREENSHOT -f
 rm $SCREENSHOT
+
+revert
 
