817204c3b8ff6d2744eb7a7cfd215df694a1df6a
Paul Irish <paul.irish@gmail.com>
2018-01-30 13:44:16 -0800

    scmpuff hacks because completion and fish scmpuff and fish 2.7

--- fish/functions/scmpuff_status.fish
+++ fish/functions/scmpuff_status.fish
@@ -1,12 +1,14 @@
-
+# https://github.com/arbelt/fish-plugin-scmpuff/blob/master/functions/scmpuff_status.fish
+# Defined in /var/folders/f1/52vyk89d2d72xh5ns6f4t_41r4mbgt/T//fish.jrYwS7/scmpuff_status.fish @ line 2
 function scmpuff_status
     scmpuff_clear_vars
     set -lx scmpuff_env_char "e"
-    set -l cmd_output (/usr/bin/env scmpuff status --filelist $argv)
+    set -l cmd_output (/usr/bin/env scmpuff status --filelist $argv ^/dev/null)
     set -l es "$status"
 
     if test $es -ne 0
-        return $es
+        git status
+        return $status
     end
 
     set -l files (string split \t $cmd_output[1])
