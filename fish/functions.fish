9e31846d5d88103660c9d3249374faf9d57a533d
Paul Irish <paul.irish@gmail.com>
2018-01-30 13:45:06 -0800

    gz function gets throusands grouping. o/

--- fish/functions.fish
+++ fish/functions.fish
@@ -45,9 +45,9 @@ end
 
 function gz --d "Get the gzipped size"
   echo "orig size    (bytes): "
-  cat "$argv[1]" | wc -c
+  cat "$argv[1]" | wc -c | gnumfmt --grouping
   echo "gzipped size (bytes): "
-  gzip -c "$argv[1]" | wc -c
+  gzip -c "$argv[1]" | wc -c | gnumfmt --grouping
 end
 
 function sudo!!
