30ab0f749ea0969831540f7b1b79aa9b14c26919
Jan Baer <jan@janbaer.de>
2018-07-22 12:24:52 +0200

    dotfiles - Cleanup unused files
    - Config for Bash
    - MacOS specific config files
    - CMUS config

--- bin/cmus_delete_current_song.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-file="$(cmus-remote -C 'echo {}')"
-
-if [ -f "$file" ]; then
-  rm "${file}" && cmus-remote -C 'win-remove'
-else
-  echo "Oop, couldn't find selected track ${file}" >&2
-fi
