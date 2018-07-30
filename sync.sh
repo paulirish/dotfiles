92d2865ccccd136f3f2def590ef12edab0e6ecfc
Jan Baer <jan@janbaer.de>
2018-07-30 18:05:17 +0200

    scripts - Fix sync script
    It wasn't substituting the home short cut

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ copyFiles() {
 copyFiles
 
 # Rename i3 configs
-if [ -f "~/.config/i3/config-${HOSTNAME}" ]; then
+if [ -f ~/.config/i3/config-${HOSTNAME} ]; then
   mv ~/.config/i3/config-${HOSTNAME} ~/.config/i3/config
   mv ~/.config/i3/i3status-${HOSTNAME}.conf ~/.config/i3/i3status.conf
 fi
