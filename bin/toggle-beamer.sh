83466cb284e564d0d877a420411642072679ebcf
Jan Baer <jan@janbaer.de>
2018-08-14 16:34:00 +0200

    scripts - toggle-beamer accepts now as second optional parameter the resolution for the external display

--- bin/toggle-beamer.sh
+++ bin/toggle-beamer.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 COMMAND=${1:-on}
+RESOLUTION=${2:-1920x1080}
+
+# To detect all possible resolutions for DP1, connect the external Beamer or monitor with the
+# notebook and enter xrandr in the console. Now you can choose one of the listed solutions
+# and pass it as the second argument when calling this script
+# example `toggle-beamer on 2560x1600`
 
 echo "Use command ${COMMAND}"
 
 if [ "${COMMAND}" == "on" ]; then
   echo "Turn output to beamer on..."
-  xrandr --output DP1 --mode 1920x1080 --above eDP1
+  xrandr --output DP1 --mode ${RESOLUTION} --above eDP1
 else
   echo "Turn output to beamer off..."
   xrandr --output DP1 --off
