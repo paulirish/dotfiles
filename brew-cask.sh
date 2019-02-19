30b59a4905d3e25098966829e367879677a50d91
Dzmitry Das <dbachko@gmail.com>
2019-02-19 11:31:27 -0500

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -16,8 +16,8 @@ brew cask install spectacle
 # brew cask install flux
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
+brew cask install hyper
+brew cask install visual-studio-code
 brew cask install atom
 brew cask install imagealpha
 brew cask install imageoptim
@@ -26,8 +26,9 @@ brew cask install sequel-pro
 brew cask install psequel
 brew cask install slack
 brew cask install docker
+brew cask install android-sdk
+brew cask install android-platform-tools
 # brew cask install java
-
 
 # fun
 # brew cask install limechat
@@ -35,11 +36,12 @@ brew cask install docker
 # brew cask install horndis               # usb tethering
 
 # browsers
-# brew cask install google-chrome-canary
-# brew cask install firefoxnightly
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install tor-browser
+brew cask install firefox-nightly
 # brew cask install webkit-nightly
 # brew cask install chromium
-# brew cask install torbrowser
 
 # less often
 # brew cask install disk-inventory-x
@@ -50,9 +52,3 @@ brew cask install transmission
 brew tap caskroom/fonts
 brew cask install font-fira-code
 brew cask install font-hack-nerd-font
-
-
-# Not on cask but I want regardless.
-
-# File Multi Tool 5
-# Phosphor
