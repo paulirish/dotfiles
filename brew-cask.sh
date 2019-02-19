11c44027cb8f13c03952f9c2bb1c1e0f72c2213a
Dzmitry Das <dbachko@gmail.com>
2019-02-19 12:20:05 -0500

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,250 +1,55 @@
-# copy paste this file in bit by bit.
-# don't run it.
-  echo "do not run this script in one go. hit ctrl-c NOW"
-  read -n 1
-
-
-##############################################################################################################
-###  backup old machine's key items
-
-mkdir -p ~/migration/home/
-mkdir -p ~/migration/Library/"Application Support"/
-mkdir -p ~/migration/Library/Preferences/
-mkdir -p ~/migration/Library/Application Support/
-mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
-
-cd ~/migration
-
-# what is worth reinstalling?
-brew leaves              > brew-list.txt    # all top-level brew installs
-brew cask list           > cask-list.txt
-npm list -g --depth=0    > npm-g-list.txt
-yarn global ls --depth=0 > yarn-g-list.txt
-
-# then compare brew-list to what's in `brew.sh`
-#   comm <(sort brew-list.txt) <(sort brew.sh-cleaned-up)
-
-# backup some dotfiles likely not under source control
-cp -Rp \
-    ~/.bash_history \
-    ~/.extra ~/.extra.fish \
-    ~/.gitconfig.local \
-    ~/.gnupg \
-    ~/.nano \
-    ~/.nanorc \
-    ~/.netrc \
-    ~/.ssh \
-    ~/.z   \
-        ~/migration/home
-
-cp -Rp ~/Documents ~/migration
-
-cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
-
-cp -Rp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration/Library/Preferences/
-cp -Rp ~/Library/Preferences/com.tinyspeck.slackmacgap.plist ~/migration/Library/Preferences/
-
-cp -Rp ~/Library/Services ~/migration/Library/ # automator stuff
-cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
-
-# editor settings & plugins
-cp -Rp ~/Library/Application\ Support/Sublime\ Text\ * ~/migration/Library/"Application Support"
-cp -Rp ~/Library/Application\ Support/Code\ -\ Insider* ~/migration/Library/"Application Support"
-
-# also consider...
-# random git branches you never pushed anywhere?
-# git untracked files (or local gitignored stuff). stuff you never added, but probably want..
-
-
-# OneTab history pages, because chrome tabs are valuable.
-
-# usage logs you've been keeping.
-
-# iTerm settings.
-  # Prefs, General, Use settings from Folder
-
-# Finder settings and TotalFinder settings
-#   Not sure how to do this yet. Really want to.
-
-# Timestats chrome extension stats
-#   chrome-extension://ejifodhjoeeenihgfpjijjmpomaphmah/options.html#_options
-# 	gotta export into JSON through devtools:
-#     copy(JSON.stringify(localStorage))
-#     pbpaste > timestats-canary.json.txt
-
-# software licenses.
-#   sublimetext's is in its Application Support folder
-
-# maybe ~/Pictures and such
-cp -Rp ~/Pictures ~/migration
-
-### end of old machine backup
-##############################################################################################################
-
-
-
-##############################################################################################################
-### XCode Command Line Tools
-#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
-
-if ! xcode-select --print-path &> /dev/null; then
-
-    # Prompt user to install the XCode Command Line Tools
-    xcode-select --install &> /dev/null
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Wait until the XCode Command Line Tools are installed
-    until xcode-select --print-path &> /dev/null; do
-        sleep 5
-    done
-
-    print_result $? 'Install XCode Command Line Tools'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Point the `xcode-select` developer directory to
-    # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
-
-    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-    print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Prompt user to agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
-
-    sudo xcodebuild -license
-    print_result $? 'Agree with the XCode Command Line Tools licence'
-
-fi
-###
-##############################################################################################################
-
-
-
-##############################################################################################################
-### homebrew!
-
-# (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
-mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
-export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
-
-# install all the things
-./brew.sh
-./brew-cask.sh
-
-### end of homebrew
-##############################################################################################################
-
-
-
-
-##############################################################################################################
-### install of common things
-###
-
-# github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-
-# Type `git open` to open the GitHub page or website for a repository.
-npm install -g git-open
-
-# fancy listing of recent branches
-npm install -g git-recent
-
-# sexy git diffs
-npm install -g diff-so-fancy
-
-# trash as the safe `rm` alternative
-npm install --global trash-cli
-
-# install better nanorc config
-# https://github.com/scopatz/nanorc
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
-
-# github.com/rupa/z   - oh how i love you
-git clone https://github.com/rupa/z.git ~/code/z
-# consider reusing your current .z file if possible. it's painful to rebuild :)
-# z is hooked up in .bash_profile
-
-
-# github.com/thebitguru/play-button-itunes-patch
-# disable itunes opening on media keys
-# git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# 	 + first move Photo Booth folder out of Pictures
-# 	 + then start Photo Booth. It'll ask where to put the library.
-# 	 + put it in Dropbox/public
-# 	* Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# 	* then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
-# Add new bash v5 to `/etc/shells`
-echo $(brew --prefix)/bin/bash | sudo tee -a /etc/shells
-
-# Add fish to `/etc/shells`
-FISHPATH=$(brew --prefix)/bin/fish
-echo $FISHPATH | sudo tee -a /etc/shells
-chsh -s $FISHPATH
-
-# Install fisher
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-# Install theme
-fisher add oh-my-fish/theme-bobthefish
-
-# iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
-#   (admittedly not as easy to maintain)
-
-
-# setting up the sublime symlink
-# ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
-
-###
-##############################################################################################################
-
-
-
-##############################################################################################################
-### remaining configuration
-###
-
-# go read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing.
-
-# prezto and antigen communties also have great stuff
-#   github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
-
-# set up osx defaults
-#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
-sh .osx
-
-# setup and run Rescuetime!
-
-###
-##############################################################################################################
-
-
-
-##############################################################################################################
-### symlinks to link dotfiles into ~/
-###
-
-#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
-#   now .gitconfig can be shared across all machines and only the .local changes
-
-# symlink it up!
-./symlink-setup.sh
-
-# add manual symlink for .ssh/config and probably .config/fish
-
-###
-##############################################################################################################
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Additional apps versions
+brew tap caskroom/versions
+
+# daily
+brew cask install spectacle
+# brew cask install gyazo
+# brew cask install rescuetime
+# brew cask install nordvpn
+# brew cask install flux
+
+# dev
+brew cask install iterm2
+brew cask install hyper
+brew cask install visual-studio-code
+brew cask install atom
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install charles
+brew cask install sequel-pro
+brew cask install psequel
+brew cask install slack
+brew cask install docker
+brew cask install android-sdk
+brew cask install android-platform-tools
+# brew cask install java
+
+# fun
+# brew cask install limechat
+# brew cask install miro-video-converter
+# brew cask install horndis               # usb tethering
+
+# browsers
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install tor-browser
+brew cask install firefox-nightly
+# brew cask install webkit-nightly
+# brew cask install chromium
+
+# less often
+# brew cask install disk-inventory-x
+# brew cask install screenflow
+brew cask install vlc
+# brew cask install licecap
+brew cask install transmission
+brew tap caskroom/fonts
+brew cask install font-fira-code
+brew cask install font-hack-nerd-font
