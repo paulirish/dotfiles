0822b864d39cd381cba7c45a5cc625d1c6f1ed3b
Jan Baer <jan@janbaer.de>
2019-05-26 08:57:11 +0200

    scripts - Remove sshpass from rsync script

--- bin/rsync-mydata.sh
+++ bin/rsync-mydata.sh
@@ -4,16 +4,14 @@ function backup {
   USER=$3
   DIRECTORIES=$4
 
-  PASSWORD=$(pass /home/JABASOFT-DS/jan)
-
   for DIRECTORY_NAME in $DIRECTORIES; do
-    sshpass -p $PASSWORD rsync -avuz --progress --delete \
+    rsync -avuz --progress --delete \
           --exclude="node_modules/" --exclude="DS_store" --exclude=".localized" \
           -e "ssh -i ~/.ssh/rsync-key -p $JABASOFT_DS_SSH_PORT" \
           ~/$DIRECTORY_NAME $USER@$SERVER_ADDRESS:/volume1/homes/$USER/$HOSTNAME/
   done
 
-  sshpass -p $PASSWORD rsync -avuz --progress --delete \
+  rsync -avuz --progress --delete \
         --exclude=".local/" --include="*.local" --exclude="*" \
         -e "ssh -i ~/.ssh/rsync-key -p $JABASOFT_DS_SSH_PORT" \
         ~/ $USER@$SERVER_ADDRESS:/volume1/homes/$USER/$HOSTNAME/
