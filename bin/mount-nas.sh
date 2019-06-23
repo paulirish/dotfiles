46d82ee184047b5a6453d4f67b6541467f7009c8
Jan Baer <jan@janbaer.de>
2019-06-23 10:16:40 +0200

    Scripts - Fix mount-nas script which was not working with Manjaro

--- bin/mount-nas.sh
+++ bin/mount-nas.sh
@@ -8,6 +8,6 @@ SUDO_PWD=$(pass /home/sudo)
 for SHARE in video music daten photo; do
   IS_MOUNTED=$(mount -ls | grep ${SHARE})
   if [ -z "${IS_MOUNTED}" ]; then
-    echo ${SUDO_PWD} | sudo -S sudo mount -t cifs //$SERVER_ADDRESS/$SHARE /media/$SHARE -o username=$USER,password=$PASSWORD
+    echo ${SUDO_PWD} | sudo -S sudo mount -t cifs //$SERVER_ADDRESS/$SHARE /media/$SHARE -o username=$USER,password=$PASSWORD,vers=1.0
   fi
 done
