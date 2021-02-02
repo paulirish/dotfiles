6a190db9cf8f7f3d596bfeba8b8463bc56681437
Paul Irish <commits@paul.irish>
2021-02-02 11:19:55 -0800

    add iterm shell integration scripts

--- fish/config.fish
+++ fish/config.fish
@@ -103,9 +103,6 @@ set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
 set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
 
-# this currently messes with newlines in my prompt. lets debug it later.
-# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
 # tabtab source for yarn package
 # uninstall by removing these lines or running `tabtab uninstall yarn`
 [ -f /Users/paulirish/.config/yarn/global/node_modules/tabtab/.completions/yarn.fish ]; and . /Users/paulirish/.config/yarn/global/node_modules/tabtab/.completions/yarn.fish
@@ -115,4 +112,9 @@ set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
  # note.. without `ag` this is a good fallback: set -gx FZF_DEFAULT_COMMAND 'fd --type f'
 set -gx FZF_DEFAULT_COMMAND 'command ag -l -g ""'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+
+# TODO debug this
+# this currently messes with newlines in my prompt. lets debug it later.
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
