ebb157a11ab28f3e9858458aeace205d4d8f5d67
Jan Baer <jan@janbaer.de>
2021-02-26 19:53:00 +0100

    sync - Fix problem with hostname

--- sync.sh
+++ sync.sh
@@ -8,10 +8,12 @@ copyFiles() {
 
 copyFiles
 
+HOST_NAME=$(cat /etc/hostname)
+
 # Rename i3 configs
-if [ -f ~/.config/i3/config-${HOSTNAME} ]; then
-  mv ~/.config/i3/config-${HOSTNAME} ~/.config/i3/config
-  mv ~/.config/i3/i3status-${HOSTNAME}.conf ~/.config/i3/i3status.conf
+if [ -f ~/.config/i3/config-${HOST_NAME} ]; then
+  mv ~/.config/i3/config-${HOST_NAME} ~/.config/i3/config
+  mv ~/.config/i3/i3status-${HOST_NAME}.conf ~/.config/i3/i3status.conf
 fi
 
 IS_I3_RUNNING=$(ps aux | grep i3 | grep -v grep)
