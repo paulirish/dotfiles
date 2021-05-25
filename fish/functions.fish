4a352a29e6992e4451fd7cc0929bd8edb592d297
Paul Irish <commits@paul.irish>
2021-05-25 11:54:29 -0700

    server cmd uses new statikk stuff. man its so good.

--- fish/functions.fish
+++ fish/functions.fish
@@ -86,23 +86,14 @@ function fuck -d 'Correct your previous console command'
     end
 end
 
-function server -d 'Start a HTTP server in the current dir, optionally specifying the port'
+# requires my excellent `npm install -g statikk`
+function server -d 'Start a HTTP server in the current dir, optionally specifying the port'    
     if test $argv[1]
         set port $argv[1]
+        statikk --open --port "$port"
     else
-        set port 8011
+        statikk --open
     end
-
-    open "http://localhost:$port/" &
-    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-#     python -c "import SimpleHTTPServer
-# map = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;
-# map[\"\"] = \"text/plain\";
-# for key, value in map.items():
-#   map[key] = value + \";charset=UTF-8\";
-#   SimpleHTTPServer.test()" $port
-    statikk --port "$port" .
 end
 
 
