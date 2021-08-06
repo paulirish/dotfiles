6cce94a4a4f0cae7784488eb0618f5cf2e3e5c1e
Mike Cuoco <mcuoco@loggimacms.salk.edu>
2021-08-06 14:51:19 -0700

    add my own stuff

--- brew-cask.sh
+++ brew-cask.sh
@@ -9,19 +9,40 @@
 
 # daily
 # brew install --cask rectangle
-brew install --cask 1password
+brew install --cask 1password 1password-cli
 brew install --cask rescuetime # time tracking
-
-# dev
-brew install --cask iterm2
-brew install --cask sublime-text
-brew install --cask imageoptim # image compression
-
-# less often
-# brew install --cask screenflow # for screen recording
-brew install --cask vlc
-
+brew install --cask mathpix-snipping-tool # for auto latex generation and OCR
+brew install --cask google-chrome
+brew install --cask google-drive google-drive-file-stream
+brew install --cask slack discord # communication
+brew install --cask zotero skim # papers
+brew install --cask alfred # system searching
 brew install --cask spotify
 
+# dev / bio
+brew install --cask iterm2
+brew install --cask sublime-text visual-studio-code # editors
+brew install --cask imageoptim # image compression
+brew install --cask rstudio # IDE
+brew install --cask imagej fiji # microscopy image stuff
+brew install --cask docker # containers!
+# Add conda??
+
+# less frequently used things
+brew install --cask screenflow # for screen recording
+brew install --cask vlc
+
+# Spacemacs
+brew tap d12frosted/emacs-plus
+brew install emacs-plus@28 --with-spacemacs-icon
+brew link emacs-plus
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# Mac App Store
+brew install mas
+mas install 1176895641 # Spark email client
+mas install 441258766 # Magnet for window control
+
+# Fonts
 brew tap homebrew/cask-fonts
-brew install --cask font-fira-code
+brew install --cask font-fira-code font-source-code-pro
