eb0986645c8d3686ba229b51e834c5e452306954
Mike Cuoco <mcuoco@mikes-logg-macbook.local>
2021-08-08 07:14:58 -0700

    add dropbox, twitter, and reorganize

--- brew-cask.sh
+++ brew-cask.sh
@@ -8,16 +8,22 @@
 # Install native apps
 
 # daily
-# brew install --cask rectangle
 brew install --cask 1password 1password-cli
 brew install --cask rescuetime # time tracking
 brew install --cask mathpix-snipping-tool # for auto latex generation and OCR
 brew install --cask google-chrome
-brew install --cask google-drive google-drive-file-stream
-brew install --cask slack discord # communication
+brew install --cask slack  # communication
+brew install --cask discord
 brew install --cask zotero skim # papers
 brew install --cask alfred # system searching
 brew install --cask spotify
+
+# torrent
+brew install --cask transmission
+
+# cloud storage
+brew install --cask google-drive google-drive-file-stream
+brew install --cask dropbox
 
 # dev / bio
 brew install --cask iterm2
@@ -42,6 +48,7 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 brew install mas
 mas install 1176895641 # Spark email client
 mas install 441258766 # Magnet for window control
+mas install 1482454543 # Twitter
 
 # Fonts
 brew tap homebrew/cask-fonts
