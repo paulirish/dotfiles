e226497a5e5e0085fde571b8b463170b62149f8d
Jan Baer <jan@janbaer.de>
2021-11-28 17:58:28 +0100

    scripts - Customize rsync to SSD for JABASOFT-MBX

--- bin/rsync-JABASOFT-MBX-to-SSD.sh
+++ bin/rsync-JABASOFT-MBX-to-SSD.sh
@@ -2,5 +2,5 @@
 
 . /home/jan/bin/rsync-mydata.sh
 
-backup "SSD" "${HOSTNAME}" "jan" ".ssh Desktop Documents Downloads Pictures Videos Music Templates Secure Projects"
+backup "SSD" "${HOSTNAME}" "jan" ".ssh Desktop Documents Downloads Pictures Videos Templates Projects"
 
