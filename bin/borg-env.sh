a8c230c05de19198b3a131dedda1b8f244b576c8
Jan Baer <jan@janbaer.de>
2022-09-06 10:34:46 +0200

    Shell: Add alias to mount latest Borg backup

--- bin/borg-env.sh
+++ bin/borg-env.sh
@@ -1,3 +1,5 @@
 export BORG_REPO="ssh://u315810@u315810.your-storagebox.de/./borg-backups/jabasoft-ds"
 export BORG_RSH="ssh -i ~/.ssh/ssh_hetzner_storagebox -p23"
 export BORG_PASSPHRASE="$(gopass show cloud/hetzner/storagebox_borg)"
+
+alias borg-mount-latest="borg mount ::$(borg list --format '{archive}{NL}' --last 1) /mnt/borg"
