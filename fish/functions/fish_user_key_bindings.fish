881e350b90106f9743d8d4739549d09f87b0ea60
Paul Irish <commits@paul.irish>
2022-12-18 14:52:48 -0800

    fzf / fd / ag stuff

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 
 # I love the FZF keybindings, especially ctrl-t midway through typing  a command
 
+# orig source is ~ https://github.com/junegunn/fzf/blob/master/shell/key-bindings.fish
 set -l fzfb_path "$(brew --prefix)/opt/fzf/shell/key-bindings.fish"
 if test -e $fzfb_path
 	source $fzfb_path
