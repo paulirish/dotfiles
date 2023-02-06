4fe7dd541f12bc5b5aecfff68c74d0f9a1804ee8
Jan Baer <jan@janbaer.de>
2023-02-06 05:45:22 +0100

    nvim - Add autocmd config for Markdown and also add custom dictionaries for DE and EN

--- .config/nvim/after/plugin/autocmd.lua
+++ .config/nvim/after/plugin/autocmd.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
-  augroup rustGroup
+  augroup rust
     autocmd!
-    autocmd FileType rust setlocal shiftwidth=2 tabstop=2 softtabstop=2 preserveindent
+    autocmd FileType rust setlocal shiftwidth=2 tabstop=2 softtabstop=2 preserveindent nowrap
     autocmd FileType rust compiler cargo
     autocmd FileType rust lua vim.keymap.set('n', '<leader>rr', '<cmd>RustRun<cr>')
     autocmd FileType rust lua vim.keymap.set('n', '<leader>rf', '<cmd>lua vim.lsp.buf.format({ async = true }<cr>'))
@@ -9,5 +9,12 @@ vim.cmd([[
     " autocmd BufWritePre *.rs lua vim.lsp.buf.format({ async = true })
     autocmd BufWinLeave *.rs mkview
     autocmd BufWinEnter *.rs silen loadview
+  augroup END
+  augroup markDown
+    autocmd!
+    autocmd FileType markdown setlocal shiftwidth=2 tabstop=2 softtabstop=2 preserveindent wrap 
+    autocmd FileType markdown setlocal spell spelllang=de,en spellfile=~/Projects/dotfiles/.config/nvim/spell/de.utf-8.add
+    autocmd BufWinLeave *.md mkview
+    autocmd BufWinEnter *.md silent loadview
   augroup END
 ]])
