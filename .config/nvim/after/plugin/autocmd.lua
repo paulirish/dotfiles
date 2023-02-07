8ab9f21ee5d759a990af88b02c861959ce1f9611
Jan Baer <jan@janbaer.de>
2023-02-07 12:51:17 +0100

    nvim: Prevent error messages for aucmd loadview and add settings for Yaml files

--- .config/nvim/after/plugin/autocmd.lua
+++ .config/nvim/after/plugin/autocmd.lua
@@ -8,13 +8,23 @@ vim.cmd([[
     " autocmd FileType rust lua vim.keymap.set('n', '<leader>rb', '<cmd>Make build<cr>')
     " autocmd BufWritePre *.rs lua vim.lsp.buf.format({ async = true })
     autocmd BufWinLeave *.rs mkview
-    autocmd BufWinEnter *.rs silent loadview
+    autocmd BufWinEnter *.rs silent! loadview
   augroup END
+
   augroup markDown
     autocmd!
     autocmd FileType markdown setlocal shiftwidth=2 tabstop=2 softtabstop=2 preserveindent wrap 
     autocmd FileType markdown setlocal spell spelllang=de,en spellfile=~/Projects/dotfiles/.config/nvim/spell/de.utf-8.add
     autocmd BufWinLeave *.md mkview
-    autocmd BufWinEnter *.md silent loadview
+    autocmd BufWinEnter *.md silent! loadview
+  augroup END
+
+  augroup yamlFile
+    autocmd!
+    autocmd BufNewFile,BufRead .ansiblelint set filetype=yaml
+    autocmd BufNewFile,BufRead .yamllint set filetype=yaml
+    autocmd Filetype yaml set cursorcolumn
+    autocmd BufWinLeave *.y*ml mkview
+    autocmd BufWinEnter *.y*ml silent! loadview
   augroup END
 ]])
