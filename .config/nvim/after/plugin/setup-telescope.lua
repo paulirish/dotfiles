ae68458c1af2bba9462a02dc877ad0f9a8e6f5e6
Jan Baer <jan@janbaer.de>
2023-02-12 19:58:48 +0100

    nvim: Fix issue, that extensions wasn't loaded

--- .config/nvim/after/plugin/setup-telescope.lua
+++ .config/nvim/after/plugin/setup-telescope.lua
@@ -3,16 +3,6 @@ if not telescope_status_ok then
   return
 end
 
-local telescope_repo_status_ok, _ = pcall(require, 'telescope-repo')
-if not telescope_repo_status_ok then
-  return
-end
-
-local telescope_fzf_status_ok, _ = pcall(require, 'telescope-fzf-native')
-if not telescope_fzf_status_ok then
-  return
-end
-
 telescope.setup {
   extensions = {
     repo = {
