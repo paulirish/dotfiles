26e7b855c14e701746dc2d9484e0f32f1bf11967
Jan Baer <jan@janbaer.de>
2023-02-26 07:24:40 +0100

    nvim: Some optimizations for null_ls for using prettier

--- .config/nvim/after/plugin/setup-null-ls.lua
+++ .config/nvim/after/plugin/setup-null-ls.lua
@@ -6,9 +6,13 @@ end
 
 null_ls.setup({
   sources = {
-    -- null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.prettier,
-    -- null_ls.builtins.completion.spell,
+    null_ls.builtins.diagnostics.ansiblelint,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.prettier.with {
+      extra_filetypes = { "toml", "yaml", "json", "markdown", "html" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
   },
 })
