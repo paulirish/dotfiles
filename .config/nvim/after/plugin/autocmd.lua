d26c86926a9a5b52dc1710dc33688218d2b32768
Jan Baer <jan@janbaer.de>
2023-02-28 17:06:21 +0100

    nvim: Configuring null_ls for autoformatting on save, when it is supported by the LSP

--- .config/nvim/after/plugin/autocmd.lua
+++ .config/nvim/after/plugin/autocmd.lua
@@ -25,8 +25,7 @@ vim.cmd([[
     autocmd FileType rust setlocal shiftwidth=2 tabstop=2 softtabstop=2 preserveindent nowrap foldmethod=indent
     autocmd FileType rust compiler cargo
     autocmd FileType rust lua vim.keymap.set('n', '<leader>rr', '<cmd>RustRunnables<cr>')
-    " autocmd FileType rust lua vim.keymap.set('n', '<leader>rf', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>')
-    autocmd BufWritePre *.rs lua vim.lsp.buf.format({ async = true })
+    autocmd BufWritePre *.rs lua vim.lsp.buf.format({ async = false })
     autocmd BufWinLeave *.rs mkview
     autocmd BufWinEnter *.rs silent! loadview
   augroup END
--- .config/nvim/after/plugin/setup-null-ls.lua
+++ .config/nvim/after/plugin/setup-null-ls.lua
@@ -4,15 +4,30 @@ if not null_ls_status_ok then
   return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.ansiblelint,
     null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.formatting.prettier.with {
       extra_filetypes = { "toml", "yaml", "json", "markdown", "html" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false, bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
