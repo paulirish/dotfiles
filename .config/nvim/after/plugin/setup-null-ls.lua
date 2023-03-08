7b038f0243743a24d2dcb6dba6922f9a76974931
Jan Baer <jan@janbaer.de>
2023-03-08 16:26:26 +0100

    nvim: Deactivate automatic reformatting while safing for all supported document types.
    It's causing unwanted changes in documents where Prettier was not used before

--- .config/nvim/after/plugin/setup-null-ls.lua
+++ .config/nvim/after/plugin/setup-null-ls.lua
@@ -1,10 +1,10 @@
--- See https://github.com/jose-elias-alvarez/null-ls.nvim 
+-- See https://github.com/jose-elias-alvarez/null-ls.nvim
 local null_ls_status_ok, null_ls = pcall(require, 'null-ls')
 if not null_ls_status_ok then
   return
 end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
@@ -18,16 +18,16 @@ null_ls.setup({
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ async = false, bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  -- if client.supports_method("textDocument/formatting") then
+  --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --   vim.api.nvim_create_autocmd("BufWritePre", {
+  --     group = augroup,
+  --     buffer = bufnr,
+  --     callback = function()
+  --       vim.lsp.buf.format({ async = false, bufnr = bufnr })
+  --     end,
+  --   })
+  -- end
+  -- end,
 })
