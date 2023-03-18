e212297187ca1cbced8ca628921e65eabdcf4681
Jan Baer <jan@janbaer.de>
2023-03-18 18:00:57 +0100

    scripts: Add more directories to rsync backup cron-job

--- bin/rsync-JABASOFT-PC2.sh
+++ bin/rsync-JABASOFT-PC2.sh
@@ -2,4 +2,4 @@
 
 . /home/jan/bin/rsync-mydata.sh
 
-backup "JABASOFT-DS" "${HOSTNAME}" "jan" ".ssh Desktop Documents Downloads Pictures Projects Videos Music Templates Secure"
+backup "JABASOFT-DS" "${HOSTNAME}" "jan" ".ssh Desktop Documents Downloads Pictures Projects Videos Music Templates Secure .kube .k9s .3T/roaming"
