9e7ce33aa253babb954367f37c84ce0b10bbad4f
Ricardo Rivas <richistron@gmail.com>
2023-05-31 01:38:29 -0700

    nvim config

---
+++ .config/nvim/lua/richistron/packer.lua
@@ -0,0 +1,45 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use("tpope/vim-fugitive")
+  use("tpope/vim-surround")
+
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use('editorconfig/editorconfig-vim')
+  use('mbbill/undotree')
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
+end)
---
+++ .config/nvim/lua/richistron/plugins_config.lua
@@ -0,0 +1,71 @@
+--theme
+vim.cmd('colorscheme rose-pine')
+
+--telescope
+require("telescope").setup {
+  defaults = {
+    file_ignore_patterns = { "^.git/", "^node_modules/", "^vendor/" }
+  },
+  pickers = {
+    live_grep = {
+      additional_args = function(opts)
+        return {"--hidden"}
+      end
+    }
+  }
+}
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {
+    "ruby",
+    -- "typescript",
+    -- "vim",
+    -- "lua",
+    -- "javascript",
+    -- "python",
+    -- "c_sharp",
+    -- "make",
+    -- "hcl",
+    -- "bash",
+    -- "dockerfile",
+    -- "gitignore",
+    -- "html",
+    -- "css",
+    -- "go",
+    -- "json",
+    -- "markdown",
+    -- "yaml"
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    --disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
