640814a3611378fc7413b1fcf1d7d2fbc9755972
Jan Baer <jan@janbaer.de>
2023-06-12 09:45:19 +0200

    nvim: Use legacy tag for fidget.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,7 +22,7 @@ require('packer').startup(function(use)
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
+      { 'j-hui/fidget.nvim', tag='legacy' },
 
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
