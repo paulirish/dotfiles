32b6dbdc938f7252245ca64eb3a14367e642233b
Ricardo Rivas <richistron@gmail.com>
2023-06-16 10:44:57 -0700

    Fixing lsp mappings

--- .config/nvim/lua/richistron/lsp.lua
+++ .config/nvim/lua/richistron/lsp.lua
@@ -13,16 +13,16 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
+--local cmp_select = {behavior = cmp.SelectBehavior.Select}
+--local cmp_mappings = lsp.defaults.cmp_mappings({
+  --['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  --['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  --['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  --["<C-Space>"] = cmp.mapping.complete(),
+--})
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+--cmp_mappings['<Tab>'] = nil
+--cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
