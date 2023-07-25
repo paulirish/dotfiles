cc4c71912161a63a25280f2a4670bbf3d474d5f9
Paul Irish <commits@paul.irish>
2023-07-25 07:10:17 -0700

    chromium tweaks

--- fish/chromium.fish
+++ fish/chromium.fish
@@ -45,7 +45,7 @@ end
 # https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md
 #                          # Avoid the startup dialog for 'Chromium wants to use your confidential information stored in "Chromium Safe Storage" in your keychain'
 #                                                  # Avoid the startup dialog for 'Do you want the application “Chromium.app” to accept incoming network connections?'
-set clutch_chrome_flags "--use-mock-keychain --disable-features=DialMediaRouteProvider"
+set clutch_chrome_flags "--use-mock-keychain --disable-features=DialMediaRouteProvider --disable-features=ProcessPerSiteUpToMainFrameThreshold"
 
 
 function cr --description "open built chromium (accepts runtime flags)"
