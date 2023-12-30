2246b25f9bf669b4a9cba857987bb9e2f9f1a6b1
Jan Baer <jan@janbaer.de>
2023-12-30 09:51:11 +0100

    nvim: Add autocmd to highlight on yank
    Also some restructuring of highlight.lua

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -61,3 +61,14 @@ vim.cmd([[
     au TextChangedI * lua require("user.debounce-cmp").debounce(1500)
   augroup END
 ]])
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
