eb5a7c2e954517312284596b66f8de4fc79ce30f
Jan Baer <jan@janbaer.de>
2024-01-08 10:32:01 +0100

    nvim: Register groups explicitely in which-key to have better namings

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -6,4 +6,18 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {},
+  config = function()
+    local wk = require("which-key")
+
+    wk.register({
+      [","] = { name = "+Hop" },
+      d = { name = "+Debug" },
+      l = { name = "+Format/Linting" },
+      n = { name = "+Neotest" },
+      s = { name = "+Lsp[s]aga" },
+      t = { name = "+Telescope" },
+      w = { name = "+Workspaces" },
+      x = { name = "+Trouble" },
+    }, { prefix = "<leader>" })
+  end
 }
