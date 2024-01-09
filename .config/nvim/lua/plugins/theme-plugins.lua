e2bd47cbcda0d65c2edd4261321081edbedab511
Jan Baer <jan@janbaer.de>
2024-01-09 05:55:12 +0100

    nvim: Add new theme catpuccin

--- .config/nvim/lua/plugins/theme-plugins.lua
+++ .config/nvim/lua/plugins/theme-plugins.lua
@@ -1,5 +1,6 @@
 return {
   { "navarasu/onedark.nvim", lazy = false },
+  { "catppuccin/nvim", event = "VeryLazy", name = "catppuccin" },
   { "folke/tokyonight.nvim", event = "VeryLazy"},
   { "NLKNguyen/papercolor-theme", event = "VeryLazy" },
   { "marko-cerovac/material.nvim", event = "VeryLazy"},
