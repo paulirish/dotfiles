8ce8ba1215bc63b73c4ec4442d347cdad78a2e85
Jan Baer <jan@janbaer.de>
2024-02-16 12:28:27 +0100

    nvim: Close nvim-tree when last window was closed
    It's using this solution for the non existing feature: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#ppwwyyxx

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -69,5 +69,22 @@ return {
     keymap.set("n", "<leader>tl", "<cmd>NvimTreeFindFile<CR>", { desc = "Find current file in file explorer" })
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+
+    vim.api.nvim_create_autocmd("QuitPre", {
+      callback = function()
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(invalid_win, w)
+          end
+        end
+        if #invalid_win == #wins - 1 then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+        end
+      end
+    })
   end,
 }
