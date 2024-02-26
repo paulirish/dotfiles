0e0c2ae8728d8ab44d4b366f49606df8eb6b4b55
Jan Baer <jan@janbaer.de>
2024-02-26 17:58:26 +0100

    nvim: Adding plugins nvim-spider to be able to navigate through partial words

---
+++ .config/nvim/lua/plugins/spider.lua
@@ -0,0 +1,27 @@
+-- Use the w, e, b motions like a spider. Move by subwords and skip insignificant punctuation.
+-- https://github.com/chrisgrieser/nvim-spider
+return {
+  "chrisgrieser/nvim-spider",
+  lazy = false,
+  config = function()
+    require("spider").setup()
+    vim.keymap.set(
+      { "n", "o", "x" },
+      "w",
+      "<cmd>lua require('spider').motion('w')<CR>",
+      { desc = "Spider-w" }
+    )
+    vim.keymap.set(
+      { "n", "o", "x" },
+      "e",
+      "<cmd>lua require('spider').motion('e')<CR>",
+      { desc = "Spider-e" }
+    )
+    vim.keymap.set(
+      { "n", "o", "x" },
+      "b",
+      "<cmd>lua require('spider').motion('b')<CR>",
+      { desc = "Spider-b" }
+    )
+  end
+}
