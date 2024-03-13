11e1bf12564a47dd092840bb890002caa87bd424
Jan Baer <jan@janbaer.de>
2024-03-13 08:42:33 +0100

    nvim: Adding new plugin fzf-lua
    Could replace the Telescope file search for me

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,25 @@
+-- Improved fzf.vim written in lua
+-- https://github.com/ibhagwan/fzf-lua
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    fzf_opts = {
+      ['--layout'] = 'reverse-list',
+    },
+    "telescope",
+    winopts = {
+      preview = {
+        default = "bat"
+      }
+    }
+  },
+  config = function(_, opts)
+    -- calling `setup` is optional for customization
+    require("fzf-lua").setup(opts)
+    vim.keymap.set("n", "<C-S-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true, desc = "Fzf files" })
+
+    vim.cmd("cnoreabbrev fzf FzfLua")
+  end
+}
