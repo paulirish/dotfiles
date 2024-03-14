a97c8bfab0cc8d00a711337d8c03d913ed568be2
Jan Baer <jan@janbaer.de>
2024-03-14 21:03:11 +0100

    nvim: Disable Harpoon for now, because I never used it

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,22 +1,26 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
+  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
   lazy = false,
   keys = {
-    { "<leader>hh", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon quick menu", },
-    { "<leader>ha", function() require("harpoon"):list():append() end, desc = "Harpoon file", },
-    { "<leader>h1", function() require("harpoon"):list():select(1) end, desc = "Harpoon to file 1", },
-    { "<leader>h2", function() require("harpoon"):list():select(2) end, desc = "Harpoon to file 2", },
-    { "<leader>h3", function() require("harpoon"):list():select(3) end, desc = "Harpoon to file 3", },
-    { "<leader>h4", function() require("harpoon"):list():select(4) end, desc = "Harpoon to file 4", },
-    { "<leader>h5", function() require("harpoon"):list():select(5) end, desc = "Harpoon to file 5", },
-    { "<leader>hp", function() require("harpoon"):list():prev() end, desc = "Harpoon previous file", },
-    { "<leader>hn", function() require("harpoon"):list():next() end, desc = "Harpoon next file", },
-    { "<leader>hr", function() require("harpoon"):list():remove() end, desc = "Harpoon remove file", },
+    { "<leader>hh", function()
+      local harpoon = require("harpoon")
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end,                                                                                                            desc = "Harpoon quick menu", },
+    { "<leader>ha", function() require("harpoon"):list():append() end,                                              desc = "Harpoon file", },
+    { "<leader>h1", function() require("harpoon"):list():select(1) end,                                             desc = "Harpoon to file 1", },
+    { "<leader>h2", function() require("harpoon"):list():select(2) end,                                             desc = "Harpoon to file 2", },
+    { "<leader>h3", function() require("harpoon"):list():select(3) end,                                             desc = "Harpoon to file 3", },
+    { "<leader>h4", function() require("harpoon"):list():select(4) end,                                             desc = "Harpoon to file 4", },
+    { "<leader>h5", function() require("harpoon"):list():select(5) end,                                             desc = "Harpoon to file 5", },
+    { "<leader>hp", function() require("harpoon"):list():prev() end,                                                desc = "Harpoon previous file", },
+    { "<leader>hn", function() require("harpoon"):list():next() end,                                                desc = "Harpoon next file", },
+    { "<leader>hr", function() require("harpoon"):list():remove() end,                                              desc = "Harpoon remove file", },
   },
   config = function()
     local harpoon = require("harpoon")
@@ -24,19 +28,19 @@ return {
 
     local conf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-        end
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
 
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-        }):find()
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
     end
 
     vim.keymap.set(
