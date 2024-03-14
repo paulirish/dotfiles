651c09821fbd8e23a52908eca050dfe7b76f5776
Jan Baer <jan@janbaer.de>
2024-03-14 21:04:10 +0100

    nvim: Some improvements for the git-signs plugin

--- .config/nvim/lua/plugins/git-signs.lua
+++ .config/nvim/lua/plugins/git-signs.lua
@@ -10,5 +10,13 @@ return {
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
     },
+    current_line_blame = true,
+    numhl = true,
   },
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
+
+    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Git preview hunk" })
+    vim.keymap.set("n", "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Git current line blame" })
+  end
 }
