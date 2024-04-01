f75e2cebbaf61f7084523caadaad3453af735fb7
Jan Baer <jan@janbaer.de>
2024-04-01 12:16:00 +0200

    scripts: Add /usr/local/bin to backup to local usb-ssd
    After I deleted one this folder accidentally, it's better to also backup
    it, at least on my local backup drive

--- bin/rsync-to-usb.sh
+++ bin/rsync-to-usb.sh
@@ -3,3 +3,4 @@
 . /home/jan/bin/rsync-mydata.sh
 
 backup "USB" "${HOSTNAME}/$(whoami)" "$(whoami)" "/home/$(whoami)" "/home/$(whoami)/rsync-local-excludes"
+backup "USB" "${HOSTNAME}/local-bin" "$(whoami)" "/usr/local/bin" "/home/$(whoami)/rsync-local-excludes"
