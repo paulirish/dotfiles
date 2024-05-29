75c93661ce45ec474cfc97566b4967fcd408d742
Jan Baer <jan@janbaer.de>
2024-05-29 13:26:28 +0200

    script: Fix mirror-display script
    The display that should be mirrored should be the last argument and the
    other monitor should be the first argument

--- bin/mirror-display.sh
+++ bin/mirror-display.sh
@@ -3,11 +3,15 @@
 # set -x
 
 mode="${1:-on}"
-monitor_name="${2:-eDP-1}"
-scale="${3:-2}"
+monitor_name="${2:-DP-1}"
+scale="${3:-auto}"
+
+internal_monitor="eDP-1"
+
+echo "Mirror to $monitor_name"
 
 if [[ "$mode" == "on" ]]; then
-  hyprctl keyword monitor DP-1,preferred,auto,${scale},mirror,${monitor_name}
+  hyprctl keyword monitor ${monitor_name},preferred,auto,${scale},mirror,${internal_monitor}
 else
-  hyprctl keyword monitor DP-1,preferred,auto,${scale}
+  hyprctl keyword monitor ${monitor_name},preferred,auto,${scale}
 fi
