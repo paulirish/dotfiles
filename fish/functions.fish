447d0105919026882433bf2079869af3be031330
Paul Irish <commits@paul.irish>
2024-10-02 07:34:31 -0700

    a nice kill_port fn using fzf

--- fish/functions.fish
+++ fish/functions.fish
@@ -21,6 +21,31 @@ function killf
   end
 end
 
+# Select a port to kill, by pid, port, or command line
+function kill_port
+
+  # Function to get the command line for a given PID
+  function get_command -a pid
+    ps -p $pid | awk 'NR>1 {for (i=4; i<=NF; i++) {printf "%s ", $i}; print ""}'
+  end
+
+  # Find listening processes, get commands, and format output
+  lsof -iTCP -sTCP:LISTEN -P | awk '{print $2, $9}' | uniq | tail -n +2 | \
+    while read -l pid port
+      set -l command (get_command $pid)
+      set -l port (string pad -w 8 (string replace 'localhost' '' $port))
+      set -l pid (string pad --right -w 6 $pid)
+      echo -e "$pid $port $command"  | column
+    end | \
+
+    # Pipe the output to fzf for selection. Grab pid and show pstree
+    fzf --exact --tac --preview 'pstree -p (echo {} | awk "{print $2}")' --preview-window=down,30% --header "Select a process to kill (PID Command Port):" | \
+
+    # Kill the selected process
+    awk '{print $1}' | xargs -r kill -9
+end
+
+
 function clone --description "clone something, cd into it. install it."
     git clone --depth=1 $argv[1]
     cd (basename $argv[1] | sed 's/.git$//')
