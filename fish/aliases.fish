f895826fa0ad4ac4d5efb8e8132780ef75f9f434
Paul Irish <commits@paul.irish>
2025-07-26 16:09:21 -0700

    zdiff3 is da best. for llm calls dont continue

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -148,7 +148,7 @@ function gemi
   #    gemi "tell me a joke"
   if test -z "$argv[1]"
     # no markdown parsing here without some real fancy stuff. because you dont want to send to markdown renderer (glow) inbetween backticks, etc.
-    llm chat --continue -m gemini-2.5-flash
+    llm chat -m gemini-2.5-flash
   else
     llm prompt -m gemini-2.5-flash "$argv" && echo "⬇️… and now rendered…⬇️" && llm logs -r | glow
   end
@@ -157,7 +157,7 @@ end
 function openai
   # using llm. same dealio as above
   if test -z "$argv[1]"
-    llm chat --continue -m gpt-4o
+    llm chat -m gpt-4o
   else
     llm prompt -m gpt-4o "$argv" && echo "⬇️… and now rendered…⬇️" && llm logs -r | glow
   end
