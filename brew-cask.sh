5d5d165a66bfb4743b902a0df22fda6eb76a95b5
Dzmitry Bachko <dbachko@gmail.com>
2026-01-30 23:12:06 -0800

    all changes

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,7 +1,4 @@
-# copy paste this file in bit by bit.
-# don't run it.
-  echo "do not run this script in one go. hit ctrl-c NOW"
-  read -n 1
+#!/bin/bash
 
 
 # to maintain cask ....
@@ -12,12 +9,12 @@
 
 # daily
 brew install --cask rectangle
-brew install --cask 1password
-brew install --cask rescuetime
+# brew install --cask 1password
+# brew install --cask rescuetime
 
 # dev
 brew install --cask iterm2
-brew install --cask sublime-text
+# brew install --cask sublime-text
 brew install --cask imageoptim
 brew install --cask docker
 brew install --cask visual-studio-code
@@ -28,8 +25,9 @@ brew install --cask codex
 brew install --cask disk-inventory-x
 brew install --cask screenflow
 brew install --cask vlc
+brew install --cask transmission
 
-brew install --cask spotify
+# brew install --cask spotify
 
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
